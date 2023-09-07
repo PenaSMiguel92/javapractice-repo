08b16f75e57414db413bbf3735f34cbea089604f
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-07 15:27:40 -0700

    solution found

--- guessnumberhigherorlower/Solution.java
+++ guessnumberhigherorlower/Solution.java
@@ -4,6 +4,21 @@ public class Solution extends GuessGame {
     }
 
     public int guessNumber(int n) {
-        return 0;
+        int start = 0;
+        int end = n;
+        return binarySearch(start, end);
+    }
+    
+    public int binarySearch(int start, int end) {
+        int val = (start + end) / 2;
+        int guessVal = guess(val);
+        if (guessVal == 0)
+            return val;
+
+        if (guessVal > 0) {
+            return binarySearch(val + 1, end);
+        } else {
+            return binarySearch(start, val);
+        }
     }
 } 
