9289104e9e4a1ae015122e79de90a20ea4a8c5e6
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-07 14:32:26 -0700

    solution found

--- uniquenoofoccurrences/Solution.java
+++ uniquenoofoccurrences/Solution.java
@@ -1,5 +1,17 @@
+import java.util.*;
 public class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        return false;
+        Map<Integer, Integer> occurMap = new HashMap<>();
+        Set<Integer> occurences = new HashSet<>();
+
+        for (int num : arr) {
+            int val = occurMap.get(num) == null ? 0 : occurMap.get(num) + 1;
+            occurMap.put(num, val);
+        }
+        for (int occur : occurMap.values())
+            if (!occurences.add(occur))
+                return false;
+
+        return true;
     }
 } 
