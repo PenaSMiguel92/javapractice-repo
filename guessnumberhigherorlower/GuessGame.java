d0eae777177a06bdf9ef2fbdfaed40ba86dd0fc7
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-07 15:04:08 -0700

    guessgame implementation

--- guessnumberhigherorlower/GuessGame.java
+++ guessnumberhigherorlower/GuessGame.java
@@ -1,4 +1,15 @@
 public class GuessGame {
-public int guess(int num) {
-}
+    private int guess_val;
+    public GuessGame(int value) {
+        this.guess_val = value;
+    }
+    public int guess(int num) {
+        if (num < this.guess_val)
+            return 1;
+
+        if (num > this.guess_val)
+            return -1;
+
+        return 0;
+    }
 }
