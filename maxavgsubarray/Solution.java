ac89733f76d59232b395b625dfc5b9fc793104e0
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-08 22:42:41 -0700

    faster implementation

--- maxavgsubarray/Solution.java
+++ maxavgsubarray/Solution.java
@@ -1,14 +1,14 @@
 public class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        int[] sum = new int[nums.length];
-        sum[0] = nums[0];
-        for (int i = 1; i < nums.length; i++)
-            sum[i] = sum[i - 1] + nums[i];
+        double sum = 0;
+        for (int i = 0; i < k; i++)
+            sum += nums[i];
 
-        double maxAvg = (sum[k-1] * 1.0) / k;
-        for (int i = k; i < nums.length; i++) 
-            maxAvg = Math.max(maxAvg, (double) (sum[i] - sum[i - k]) / k);
-
-        return maxAvg;
+        double maxAvg = sum;
+        for (int i = k; i < nums.length; i++) {
+            sum += nums[i] - nums[i - k];
+            maxAvg = Math.max(maxAvg, sum);
+        }
+        return maxAvg/k;
     }
 }
