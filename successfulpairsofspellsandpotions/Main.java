824590cc2136ffd92ebc4909bc6515ea8ae9dc3d
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-08 20:52:54 -0700

    first attempt, linear time lower_bound

--- successfulpairsofspellsandpotions/Main.java
+++ successfulpairsofspellsandpotions/Main.java
@@ -1,5 +1,8 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        
+        Solution sol = new Solution();
+        System.out.println(Arrays.toString(sol.successfulPairs(new int[] { 5, 1, 3 }, new int[] { 1, 2, 3, 4, 5 }, 7)));
+        System.out.println(Arrays.toString(sol.successfulPairs(new int[] { 3, 1, 2 }, new int[] { 8, 5, 8 }, 16)));
     }
 }
