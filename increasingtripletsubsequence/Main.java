2f05f47c261d7d685e208a10f036c23050510dbc
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-17 14:55:42 -0700

    main imp

--- increasingtripletsubsequence/Main.java
+++ increasingtripletsubsequence/Main.java
@@ -1,4 +1,9 @@
 public class Main {
-public static void main(String[] args) {
-}
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.increasingTriplet(new int[] { 1, 2, 3, 4, 5 }));
+        System.out.println(sol.increasingTriplet(new int[] { 5, 4, 3, 2, 1 }));
+        System.out.println(sol.increasingTriplet(new int[] {2, 1, 5, 0, 4, 6}));
+
+    }
 }
--- increasingtripletsubsequence/Solution.java
+++ increasingtripletsubsequence/Solution.java
@@ -1,4 +1,5 @@
 public class Solution {
-public boolean increasingTriplet(int[] nums) {
-}
+    public boolean increasingTriplet(int[] nums) {
+    
+    }
 }
