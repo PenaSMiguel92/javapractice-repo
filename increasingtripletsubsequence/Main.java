e00476b9a8f2038cc0c70bf6c76f9393cbcc10fb
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-17 15:30:10 -0700

    first attempt

--- increasingtripletsubsequence/Main.java
+++ increasingtripletsubsequence/Main.java
@@ -3,7 +3,11 @@ public class Main {
         Solution sol = new Solution();
         System.out.println(sol.increasingTriplet(new int[] { 1, 2, 3, 4, 5 }));
         System.out.println(sol.increasingTriplet(new int[] { 5, 4, 3, 2, 1 }));
-        System.out.println(sol.increasingTriplet(new int[] {2, 1, 5, 0, 4, 6}));
+        System.out.println(sol.increasingTriplet(new int[] { 2, 1, 5, 0, 4, 6 }));
+        System.out.println(sol.increasingTriplet(new int[] { 20, 100, 10, 12, 5, 13 }));
+        System.out.println(sol.increasingTriplet(new int[] {1, 5, 0, 4, 1, 3});
+        System.out.println(sol.increasingTriplet(new int[] {1}));
+        System.out.println(sol.increasingTriplet(new int[] {0, 4, 2, 1, 0, -1, -3}));
 
     }
 }
--- increasingtripletsubsequence/Solution.java
+++ increasingtripletsubsequence/Solution.java
@@ -1,5 +1,22 @@
 public class Solution {
     public boolean increasingTriplet(int[] nums) {
-    
+        int i = 0;
+        int j = 0;
+        for (int k = 0; k < nums.length; k++) {
+            if ((i < j) && (j < k) && (nums[i] < nums[j]) && (nums[j] < nums[k]))
+                return true;
+
+            if ((i == j) && (nums[k] > nums[j]))
+                j = k;
+
+            if ((nums[j] > nums[i]))
+                i = Math.min(i, j);
+            else
+                i = j;
+                
+            
+        }
+        
+        return false;
     }
 }
