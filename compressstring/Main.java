f3808b85da9a0c756bdc399f9650e9fffc6b3dbe
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-17 19:34:30 -0700

    final attempt

--- compressstring/Main.java
+++ compressstring/Main.java
@@ -7,6 +7,12 @@ public class Main {
         int ans2 = sol.compress(ca2);
         char[] ca3 = new char[] { 'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b' };
         int ans3 = sol.compress(ca3);
+        char[] ca4 = new char[] {'a', 'b', 'c'};
+        int ans4 = sol.compress(ca4);
+        char[] ca5 = new char[] {'a', 'a', 'a', 'a', 'a', 'b'};
+        int ans5 = sol.compress(ca5);
+        char[] ca6 = new char[] { 'a', 'a', 'a', 'b', 'b', 'a', 'a' };
+        int ans6 = sol.compress(ca6);
 
         System.out.println("Case 1: ");
         for (int i = 0; i < ans1; i++)
@@ -23,5 +29,24 @@ public class Main {
             System.out.print(ca3[i]);
 
         System.out.print("\n");
+
+        System.out.println("Case 4: ");
+        for (int i = 0; i < ans4; i++)
+            System.out.print(ca4[i]);
+
+        System.out.print("\n");
+
+        System.out.println("Case 5: ");
+        for (int i = 0; i < ans5; i++)
+            System.out.print(ca5[i]);
+
+        System.out.print("\n");
+
+        System.out.println("Case 6: ");
+        for (int i = 0; i < ans6; i++)
+            System.out.print(ca6[i]);
+
+        System.out.print("\n");
+
     }
 }
--- compressstring/Solution.java
+++ compressstring/Solution.java
@@ -7,20 +7,27 @@ public class Solution {
         int result = 0;
         int groupLength = 0;
         for (int i = 0; i < chars.length; i++) {
+            
             if ((chars[index] != chars[i]) || (i == chars.length - 1)) {
-                groupLength = (i == chars.length - 1) ? groupLength + 1 : groupLength;
+                groupLength = ((chars[index] == chars[i]) && (i == chars.length - 1)) ? groupLength + 1 : groupLength;
                 chars[result++] = chars[index];
                 if (groupLength > 1 && groupLength < 10)
                     chars[result++] = (char) ('0' + groupLength);
                 else if (groupLength > 9)
                     for (char c : Integer.toString(groupLength).toCharArray())
                         chars[result++] = c;
+                
+                
 
                 groupLength = 1;
                 index = i;
             } else {
                 groupLength++;
             }
+
+            if ((index > 1) && (chars[index-1] != chars[i]) && (i == chars.length - 1) && (groupLength == 1)) 
+                    chars[result++] = chars[i];
+            
         }
         
         return result;
