830212683ee96048164ca869565ca49a8dcf5d23
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-17 17:45:08 -0700

    main imp

--- compressstring/Main.java
+++ compressstring/Main.java
@@ -1,4 +1,27 @@
 public class Main {
-public static void main(String[] args) {
-}
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        char[] ca1 = new char[] { 'a', 'a', 'b', 'b', 'c', 'c', 'c' };
+        int ans1 = sol.compress(ca1);
+        char[] ca2 = new char[] { 'a' };
+        int ans2 = sol.compress(ca2);
+        char[] ca3 = new char[] { 'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b' };
+        int ans3 = sol.compress(ca3);
+
+        System.out.println("Case 1: ");
+        for (int i = 0; i < ans1; i++)
+            System.out.print(ca1[ans1]);
+
+        System.out.print("\n");
+        System.out.println("Case 2: ");
+        for (int i = 0; i < ans2; i++)
+            System.out.print(ca2[ans2]);
+        
+        System.out.print("\n");
+        System.out.println("Case 3: ");
+        for (int i = 0; i < ans3; i++)
+            System.out.print(ca3[ans3]);
+
+        System.out.print("\n");
+    }
 }
--- compressstring/Solution.java
+++ compressstring/Solution.java
@@ -1,4 +1,5 @@
 public class Solution {
-public int compress(char[] chars) {
-}
+    public int compress(char[] chars) {
+    
+    }
 }
