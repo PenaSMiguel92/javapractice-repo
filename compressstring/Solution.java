00ab4c64753395132b1386545bfee3bcddaa7777
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-17 18:10:20 -0700

    solution found

--- compressstring/Solution.java
+++ compressstring/Solution.java
@@ -1,5 +1,24 @@
 public class Solution {
     public int compress(char[] chars) {
-    
+        int index = 0;
+        int result = 0;
+        int groupLength = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[index] != chars[i]) {
+                chars[result++] = chars[index];
+                if (groupLength > 1 && groupLength < 10)
+                    chars[result++] = (char) ('0' + groupLength);
+                else if (groupLength > 9)
+                    for (char c : Integer.toString(groupLength).toCharArray())
+                        chars[result++] = c;
+
+                groupLength = 0;
+                index = i;
+            } else {
+                groupLength++;
+            }
+        }
+        
+        return result;
     }
 }
