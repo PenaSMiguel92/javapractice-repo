a726f007b6a264611582618ce01ca6d1bd6ccb3a
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-18 18:15:16 -0700

    solution found

--- issubsequence/Solution.java
+++ issubsequence/Solution.java
@@ -1,4 +1,16 @@
 public class Solution {
-public boolean isSubsequence(String s, String t) {
-}
+    public boolean isSubsequence(String s, String t) {
+        int n1 = s.length();
+        int n2 = t.length();
+        int subIndex = 0;
+        for (int index = 0; index < n2; index++) {
+            if (subIndex >= n1)
+                return true;
+
+            if (t.charAt(index) == s.charAt(subIndex))
+                subIndex++;
+
+        }
+        return false;
+    }
 }
