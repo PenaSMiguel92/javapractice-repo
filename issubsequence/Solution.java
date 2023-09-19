6b897826087fe7b9c82c398f0c87718e1edec202
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-18 18:25:31 -0700

    added edge cases

--- issubsequence/Solution.java
+++ issubsequence/Solution.java
@@ -1,12 +1,16 @@
 public class Solution {
     public boolean isSubsequence(String s, String t) {
+        if (s.length() < 1 ) 
+            return true;
+        if (s.length() > t.length())
+            return false;
         int n1 = s.length();
         int n2 = t.length();
         int subIndex = 0;
         for (int index = 0; index < n2; index++) {
             if (t.charAt(index) == s.charAt(subIndex))
                 subIndex++;
-                
+
             if (subIndex >= n1)
                 return true;
 
