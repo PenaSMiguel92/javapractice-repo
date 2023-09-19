97d87e97521e258957b7784bfdc55eb915c82044
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-18 18:34:57 -0700

    main imp

--- pivotindex/Main.java
+++ pivotindex/Main.java
@@ -1,4 +1,8 @@
 public class Main {
-public static void main(String[] args) {
-}
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.pivotIndex(new int[] { 1, 7, 3, 6, 5, 6 }));
+        System.out.println(sol.pivotIndex(new int[] { 1, 2, 3 }));
+        System.out.println(sol.pivotIndex(new int[] {2, 1, -1}));
+    }
 }
