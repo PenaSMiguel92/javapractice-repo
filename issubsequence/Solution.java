2ae170ef98cd7db4e61c00bfaef5bed9218d8a99
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-18 18:19:34 -0700

    fix implemented

--- issubsequence/Solution.java
+++ issubsequence/Solution.java
@@ -4,11 +4,13 @@ public class Solution {
         int n2 = t.length();
         int subIndex = 0;
         for (int index = 0; index < n2; index++) {
+            if (t.charAt(index) == s.charAt(subIndex))
+                subIndex++;
+                
             if (subIndex >= n1)
                 return true;
 
-            if (t.charAt(index) == s.charAt(subIndex))
-                subIndex++;
+            
 
         }
         return false;
