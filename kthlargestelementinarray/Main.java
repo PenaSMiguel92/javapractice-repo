a39de32fe706d03dd1443c6c0ceb80c1f61045ac
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-20 15:40:52 -0700

    main imp

--- kthlargestelementinarray/Main.java
+++ kthlargestelementinarray/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-    
+        Solution sol = new Solution();
+        System.out.println(sol.findKthLargest(new int[] { 3, 2, 1, 5, 6, 4 }, 2));
+        System.out.println(sol.findKthLargest(new int[] {3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
     }
 }
