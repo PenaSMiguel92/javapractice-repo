10a7730ee17487b657786f6060f90c617c6b0ba7
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-20 15:39:32 -0700

    solution found

--- kthlargestelementinarray/Solution.java
+++ kthlargestelementinarray/Solution.java
@@ -1,4 +1,15 @@
+import java.util.Queue;
+import java.util.PriorityQueue;
+
 public class Solution {
-public int findKthLargest(int[] nums, int k) {
-}
+    public int findKthLargest(int[] nums, int k) {
+        Queue<Integer> numsQueue = new PriorityQueue<>();
+        for (int num : nums) {
+            numsQueue.add(num);
+        }
+        for (int i = 0; i < k-1; i++) {
+            numsQueue.poll();
+        }
+        return numsQueue.poll();
+    }
 }
