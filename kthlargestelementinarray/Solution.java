7ffdeae138e4c3113da27ca78af96adc9585c4d2
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-20 15:48:44 -0700

    compare lambda implemented

--- kthlargestelementinarray/Solution.java
+++ kthlargestelementinarray/Solution.java
@@ -3,7 +3,14 @@ import java.util.PriorityQueue;
 
 public class Solution {
     public int findKthLargest(int[] nums, int k) {
-        Queue<Integer> numsQueue = new PriorityQueue<>();
+        Queue<Integer> numsQueue = new PriorityQueue<>((a,b) -> {
+            if (a < b)
+                return 1;
+            else if (a > b)
+                return -1;
+            else
+                return 0;
+        });
         for (int num : nums) {
             numsQueue.add(num);
         }
