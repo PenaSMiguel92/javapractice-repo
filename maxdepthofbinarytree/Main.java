8d88d8bf6b6ab5ce63bb56d5bd1d62e9167d0248
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-20 14:02:15 -0700

    main imp

--- maxdepthofbinarytree/Main.java
+++ maxdepthofbinarytree/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         TreeNode root1 = TreeNode.generateTreeFromArray(new String[] { "3", "9", "20", "null", "null", "15", "7" });
-        System.out.println(root1);
         TreeNode root2 = TreeNode.generateTreeFromArray(new String[] { "1", "null", "2" });
-        System.out.println(root2);
+        Solution sol = new Solution();
+        System.out.println(sol.maxDepth(root1));
+        System.out.println(sol.maxDepth(root2));
     }
 } 
