8b35e2cf740cfbe4183191c488fd4ea3d55cc8f0
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-20 14:12:55 -0700

    solution found

--- maxdepthofbinarytree/Solution.java
+++ maxdepthofbinarytree/Solution.java
@@ -1,5 +1,15 @@
 public class Solution {
+
     public int maxDepth(TreeNode root) {
-        return -1;
+        
+        return depthCount(0, root);
+    }
+
+    public int depthCount(int current, TreeNode root) {
+        if (root == null)
+            return current;
+        current++;
+        current = Math.max(depthCount(current, root.left), depthCount(current, root.right));
+        return current;
     }
 } 
