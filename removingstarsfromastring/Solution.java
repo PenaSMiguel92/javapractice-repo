360d1e0fa24fa2824d9282651adfe5d7c3fe0ce3
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-20 16:53:14 -0700

    solution found

--- removingstarsfromastring/Solution.java
+++ removingstarsfromastring/Solution.java
@@ -1,5 +1,23 @@
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+
 public class Solution {
     public String removeStars(String s) {
-        return "";
+        char[] ans;
+        Deque<Character> charStack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c != '*')
+                charStack.push(c);
+            else
+                charStack.pop();
+        }
+        ans = new char[charStack.size()];
+        for (int i = ans.length - 1; i >= 0; i--){
+            ans[i] = (char) charStack.pop();
+        }
+        
+        return String.copyValueOf(ans);
     }
 } 
