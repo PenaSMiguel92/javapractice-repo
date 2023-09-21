15e28b572dd917e31bcd4b4c37e9c2535259c2f7
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-20 17:53:48 -0700

    first attempt

--- asteroidcollision_2/Solution.java
+++ asteroidcollision_2/Solution.java
@@ -1,5 +1,29 @@
+import java.util.Deque;
+import java.util.ArrayDeque;
+
 public class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        return new int[] {};
+        Deque<Integer> asteroidDeque = new ArrayDeque<>();
+        asteroidDeque.offer(asteroids[0]);
+        for (int i = 1; i < asteroids.length; i++) {
+            int curVal = asteroids[i];
+            if (curVal > 0) {
+                asteroidDeque.offer(curVal);
+                continue;
+            }
+
+            int last = asteroidDeque.peek();
+            if (last > 0 && Math.abs(last) <= Math.abs(curVal)) {
+                asteroidDeque.pop();
+                asteroidDeque.push(curVal);
+            } else
+                asteroidDeque.push(curVal);
+        }
+        int n = asteroidDeque.size();
+        int[] ans = new int[n];
+        for (int i = n - 1; i >= 0; i--)
+            ans[i] = asteroidDeque.pop();
+            
+        return ans;
     }
 } 
