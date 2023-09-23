0964e012f78b4a45f537a04a6dc4400af53aedb1
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-22 22:07:27 -0700

    main imp

--- medianofmergedsortedarrays/Main.java
+++ medianofmergedsortedarrays/Main.java
@@ -1,4 +1,7 @@
 public class Main {
-public static void main(String[] args) {
-}
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.findMedianSortedArrays(new int[] { 1, 3 }, new int[] { 2 }));
+        System.out.println(sol.findMedianSortedArrays(new int[] { 1, 2 }, new int[] { 3, 4 }));
+    }
 } 
