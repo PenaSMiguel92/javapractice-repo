a045449a55f45386c3687f7aabca5e6bcaa2d09a
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-24 15:55:08 -0700

    initial solution

--- SmallestNumberInInfiniteSet/SmallestInfiniteSet.java
+++ SmallestNumberInInfiniteSet/SmallestInfiniteSet.java
@@ -1,10 +1,31 @@
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.PriorityQueue;
+
 public class SmallestInfiniteSet {
-public SmallestInfiniteSet() {
-}
+    Set<Integer> isPresent;
+    Queue<Integer> addedIntegers;
+    int currentInteger = 1;
+    public SmallestInfiniteSet() {
+        this.isPresent = new HashSet<Integer>();
+        this.addedIntegers = new PriorityQueue<>();
+    }
 
-public int popSmallest() {
-}
+    public int popSmallest() {
+        if (this.addedIntegers.size() < 1)
+            return currentInteger++;
 
-public void addBack(int num) {
-}
+        int answer = this.addedIntegers.poll();
+        this.isPresent.remove(answer);
+        return answer;
+    }
+
+    public void addBack(int num) {
+        if (num >= currentInteger || this.isPresent.contains(num))
+            return;
+        
+        this.addedIntegers.offer(num);
+        this.isPresent.add(num);
+    }
 }
