f94b6813bc96ad3332e8f0f61a3c8609e5d9557c
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-24 16:52:20 -0700

    main imp

--- HouseRobber/Main.java
+++ HouseRobber/Main.java
@@ -1,4 +1,7 @@
 public class Main {
-public static void main(String[] args) {
-}
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.rob(new int[] { 1, 2, 3, 1 }));
+        System.out.println(sol.rob(new int[] {2, 7, 9, 3, 1}));
+    }
 }
