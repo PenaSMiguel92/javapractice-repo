34c81611c9fc7cf6a34e11f9ad133a19cbb7276b
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-10-05 20:34:44 -0700

    solution found

--- oddevenlinkedlist/Solution.java
+++ oddevenlinkedlist/Solution.java
@@ -1,4 +1,21 @@
 public class Solution {
-public ListNode oddEvenList(ListNode head) {
-}
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+         
+        ListNode even_start = head.next;
+        ListNode odd = head;
+        ListNode even = head.next;
+
+        while (odd.next != null && even.next != null) {
+            odd.next = even.next;
+            even.next = odd.next.next;
+            odd = odd.next;
+            even = even.next;
+        }
+        
+        odd.next = even_start;
+        return head;
+    }
 }
