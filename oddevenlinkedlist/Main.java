72aa1d7dd52560a93d1c938aaead00de3b58b5fb
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-10-05 20:02:18 -0700

    main imp

--- oddevenlinkedlist/Main.java
+++ oddevenlinkedlist/Main.java
@@ -1,4 +1,11 @@
 public class Main {
-public static void main(String[] args) {
-}
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        ListNode list1 = ListNode.generateListFromArray(new int[] { 1, 2, 3, 4, 5 });
+        ListNode list2 = ListNode.generateListFromArray(new int[] { 2, 1, 3, 5, 6, 4, 7 });
+
+
+        System.out.println(sol.oddEvenList(list1).toString());
+        System.out.println(sol.oddEvenList(list2).toString());
+    }
 }
