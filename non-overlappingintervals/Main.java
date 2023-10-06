db6cba2dc8f184f379c973faead3d513dc324470
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-10-05 19:13:54 -0700

    main imp

--- non-overlappingintervals/Main.java
+++ non-overlappingintervals/Main.java
@@ -1,4 +1,8 @@
 public class Main {
-public static void main(String[] args) {
-}
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.eraseOverlapIntervals(new int[][] { { 1, 2 }, { 2, 3 }, { 3, 4 }, { 1, 3 } }));
+        System.out.println(sol.eraseOverlapIntervals(new int[][] { { 1, 2 }, { 1, 2 }, { 1, 2 } }));
+        System.out.println(sol.eraseOverlapIntervals(new int[][] {{1, 2}, {2, 3}}));
+    }
 }
--- non-overlappingintervals/Solution.java
+++ non-overlappingintervals/Solution.java
@@ -1,4 +1,8 @@
+import java.util.Arrays;
+
 public class Solution {
-public int eraseOverlapIntervals(int[][] intervals) {
-}
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+        return 1;
+    }
 }
