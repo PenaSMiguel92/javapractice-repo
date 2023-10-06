34e85d4afe5dbdf62d13145ed84968637a61d994
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-10-05 19:55:54 -0700

    tostring util imp

--- oddevenlinkedlist/ListNode.java
+++ oddevenlinkedlist/ListNode.java
@@ -1,7 +1,30 @@
 public class ListNode {
-int val;
-ListNode next;
-ListNode() {}}
-ListNode(int val) {this.val = val;}
-ListNode(int val, ListNode next) {this.val = val; this.next = next;}
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+    
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        String val = "[";
+        val += iterateOverList(this, val);
+        return val + "]";
+    }
+
+    public static String iterateOverList(ListNode current, String val) {
+        if (current.next == null) {
+            return val + " " + String.valueOf(current.val);
+        }
+        return iterateOverList(current.next, val);
+    }
 }
