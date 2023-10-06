6048ce265d799782fb25d77bcecbc17125c5d214
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-10-05 18:54:26 -0700

    solution found

--- MinimumNumberOfArrowsToBurstBalloons/Main.java
+++ MinimumNumberOfArrowsToBurstBalloons/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println(
                 sol.findMinArrowShots(new int[][] { { -2147483646, -2147483645 }, { 2147483646, 2147483647 } }));
         System.out.println(sol.findMinArrowShots(new int[][] { { 2, 3 }, { 2, 3 } }));
-        System.out.println(sol.findMinArrowShots(new int[][] {{1, 2}}));
+        System.out.println(sol.findMinArrowShots(new int[][] { { 1, 2 } }));
+        System.out.println(sol.findMinArrowShots(new int[][] {{77171087,133597895},{45117276,135064454},{80695788,90089372},{91705403,110208054},{52392754,127005153},{53999932,118094992},{11549676,55543044},{43947739,128157751},{55636226,105334812},{69348094,125645633}}));
     }
 }
