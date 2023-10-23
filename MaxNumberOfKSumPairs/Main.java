629dca050fabea1749117f4ec494872d50ac7253
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-10-23 12:56:35 -0700

    main imp

--- MaxNumberOfKSumPairs/Main.java
+++ MaxNumberOfKSumPairs/Main.java
@@ -1,4 +1,7 @@
 public class Main {
-public static void main(String[] args) {
-}
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.maxOperations(new int[] { 1, 2, 3, 4 }, 5));
+        System.out.println(sol.maxOperations(new int[] {3, 1, 3, 4, 3}, 6));
+    }
 }
--- MaxNumberOfKSumPairs/Solution.java
+++ MaxNumberOfKSumPairs/Solution.java
@@ -1,4 +1,5 @@
 public class Solution {
-public int maxOperations(int[] nums, int k) {
-}
+    public int maxOperations(int[] nums, int k) {
+        return -1;
+    }
 }
