90ac41346a8be8b4483075011a707234e78c178a
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-10-23 12:42:16 -0700

    solution found

--- ContainerWithMostWater/Solution.java
+++ ContainerWithMostWater/Solution.java
@@ -1,5 +1,20 @@
 public class Solution {
     public int maxArea(int[] height) {
-        return -1;
+        int max = Integer.MIN_VALUE;
+        int left = 0, right = height.length - 1;
+        int leftValue = height[left], rightValue = height[right];
+        while (left < right) {
+            
+
+            int area = Math.min(leftValue, rightValue) * (right - left);
+            max = Math.max(area, max);
+
+            if (leftValue < rightValue) 
+                leftValue = height[++left];
+            else
+                rightValue = height[--right];
+        }
+
+        return max;
     }
 }
