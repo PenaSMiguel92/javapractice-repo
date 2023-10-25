8d95413be4b55ab9d4429a7bc28d11a88804704a
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-10-25 14:12:02 -0700

    solution found

--- DailyTemperatures/Solution.java
+++ DailyTemperatures/Solution.java
@@ -1,4 +1,25 @@
+import java.util.*;
+
 public class Solution {
-public int[] dailyTemperatures(int[] temperatures) {
-}
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] ans = new int[temperatures.length];
+        Deque<Integer> stack = new ArrayDeque<>(temperatures.length);
+
+        for (int i = temperatures.length - 1; i >= 0; i--) {
+            if (stack.isEmpty()) {
+                ans[i] = 0;
+                stack.push(i);
+                continue;
+            }
+            while (!stack.isEmpty()) {
+                int index = stack.pop();
+                if (temperatures[index] > temperatures[i]) {
+                    ans[i] = index - i;
+                    break;
+                }
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
 }
