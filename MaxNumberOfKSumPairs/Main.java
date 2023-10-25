c48c26959ffeef38404e7727694ad6a9bcc6da40
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-10-25 13:53:55 -0700

    solution found

--- MaxNumberOfKSumPairs/Main.java
+++ MaxNumberOfKSumPairs/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
         System.out.println(sol.maxOperations(new int[] { 1, 2, 3, 4 }, 5));
-        System.out.println(sol.maxOperations(new int[] {3, 1, 3, 4, 3}, 6));
+        System.out.println(sol.maxOperations(new int[] { 3, 1, 3, 4, 3 }, 6));
+        System.out.println(sol.maxOperations(new int[] {2, 5, 4, 4, 1, 3, 4, 4, 1, 4, 4, 1, 2, 1, 2, 2, 3, 2, 4, 2}, 3));
     }
 }
--- MaxNumberOfKSumPairs/Solution.java
+++ MaxNumberOfKSumPairs/Solution.java
@@ -1,31 +1,23 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class Solution {
     public int maxOperations(int[] nums, int k) {
         if (nums.length <= 1)
             return 0;
-
-        List<Integer> numList = new ArrayList<>(nums.length);
         Arrays.sort(nums);
-        for (int num : nums)
-            numList.add(num);
-        int operations = 0, current = 0, comp = 0;
-        while (!numList.isEmpty()) {
-            
-            if (numList.contains(k - numList.get(current))) {
-                comp = numList.lastIndexOf(k - numList.get(current));
-                if (comp == current)
-                    break;
-                numList.remove(comp);
-                numList.remove(current);
-                operations++;
-                current = 0;
-            } else {
-                current++;
-            } 
+        int i = 0, j = nums.length - 1;
+        int count = 0;
+        while (i < j) {
+            if (nums[i] + nums[j] == k) {
+                count++;
+                i++;
+                j--;
+            } else if (nums[i] + nums[j] > k) {
+                j--;
+            } else if (nums[i] + nums[j] < k) {
+                i++;
+            }
         }
-        return operations;
+        return count;
     }
 }
