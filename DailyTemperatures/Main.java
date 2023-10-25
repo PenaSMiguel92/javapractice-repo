ad50001ab93d93c1c5c6f80fa9e9c4930e673e47
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-10-25 14:14:06 -0700

    main imp

--- DailyTemperatures/Main.java
+++ DailyTemperatures/Main.java
@@ -1,5 +1,9 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-    
+        Solution sol = new Solution();
+        System.out.println(Arrays.toString(sol.dailyTemperatures(new int[] {73, 74, 75, 71, 69, 72, 76, 73})));
+        System.out.println(Arrays.toString(sol.dailyTemperatures(new int[] {30, 40, 50, 60})));
+        System.out.println(Arrays.toString(sol.dailyTemperatures(new int[] {30, 60, 90})));
     }
 }
