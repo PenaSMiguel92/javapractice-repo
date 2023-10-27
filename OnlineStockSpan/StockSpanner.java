0eda906a3a02d14b1c84db15ef5378e64301e78b
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-10-26 23:20:13 -0700

    solution found

--- OnlineStockSpan/StockSpanner.java
+++ OnlineStockSpan/StockSpanner.java
@@ -1,6 +1,18 @@
+import java.util.*;
+
 public class StockSpanner {
-public StockSpanner() {
-}
-public int next(int price) {
-}
+    private final Deque<Integer[]> span;
+
+    public StockSpanner() {
+        span = new ArrayDeque<>();
+    }
+
+    public int next(int price) {
+        int sum = 1;
+        while (!span.isEmpty() && span.peek()[0] <= price) {
+            sum += span.pop()[1];
+        }
+        span.push(new Integer[] { price, sum });
+        return sum;
+    }
 }
