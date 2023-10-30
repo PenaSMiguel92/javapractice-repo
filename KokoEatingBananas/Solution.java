d9fd08d65d5a0af7a4d4436d0b0b5ffa147b8204
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-10-30 15:54:01 -0700

    solution found

--- KokoEatingBananas/Solution.java
+++ KokoEatingBananas/Solution.java
@@ -1,5 +1,24 @@
 public class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        return -1;
+        int lo = 1, hi = -1;
+        for (int i : piles) {
+            hi = Math.max(i, hi);
+        }
+        while (lo < hi) {
+            int m = lo + (hi - lo) / 2;
+            int hrs = 0;
+            for (int amount : piles) {
+                if (amount % m == 0) {
+                    hrs += amount / m;
+                } else {
+                    hrs += amount / m + 1;
+                }
+            }
+            if (hrs > h)
+                lo = m + 1;
+            else 
+                hi = m;
+        }
+        return lo;
     }
 }
